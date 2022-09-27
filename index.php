a3da85ef9d8a41f7c009885e995278761118797d
Hoahc <hoahigh.1109@gmail.com>
2022-09-27 14:26:42 +0700

    read users json file and display data in bootstrap table

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+    require 'users.php';
+    $users = getUser();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    
+<div class="container" style="width:1000px;">  
+    <a href="">Create New User</a>
+    <div class="table-container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Website</th>
+                    <th scope="col">Extension</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) { ?>
+                <tr>
+                    <td><?= $user->id; ?></td>
+                    <td><?= $user->name; ?></td>
+                    <td><?= $user->username; ?></td>
+                    <td><?= $user->email; ?></td>
+                    <td><?= $user->phone; ?></td>
+                    <td><?= $user->website; ?></td>
+                    <td><?= $user->extension; ?></td>
+                    <td>
+                        <a href="">View</a>
+                        <a href="">Edit</a>
+                        <a href="">Delete</a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
